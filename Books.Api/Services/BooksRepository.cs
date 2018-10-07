fa08d6ce961d3233dd7341adc751bf7735cf1391
Matt Wagner <matt.wagner@tcgplayer.com>
2018-10-07 11:31:13 -0400

    Implement the Books repository

---
+++ Books.Api/Services/BooksRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Books.Api.Contexts;
+using Books.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Books.Api.Services
+{
+    public class BooksRepository : IBooksRepository, IDisposable
+    {
+        private BooksContext _context;
+
+        public BooksRepository(BooksContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<Book> GetBookAsync(Guid id)
+        {
+            return await _context.Books.Include(b => b.Author)
+                .FirstOrDefaultAsync(b => b.Id == id);
+        }
+
+        public async Task<IEnumerable<Book>> GetBooksAsync()
+        {
+            return await _context.Books.Include(b => b.Author).ToListAsync();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            // Ensure that the CLR doesn't call finalize for our repository
+            // (tell the garbage collector that this repository has already been cleaned up)
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_context != null)
+                {
+                    _context.Dispose();
+                    _context = null;
+                }
+            }
+        }
+    }
+}
