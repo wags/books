bf420c9954597b6e9779a1b993a273479993e86f
Matt Wagner <matt.wagner@tcgplayer.com>
2018-10-07 12:48:51 -0400

    Add BooksController

---
+++ Books.Api/Controllers/BooksController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Books.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Books.Api.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private IBooksRepository _booksRepository;
+
+        public BooksController(IBooksRepository booksRepository)
+        {
+            _booksRepository = booksRepository
+                ?? throw new ArgumentNullException(nameof(booksRepository));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBooks()
+        {
+            var bookEntities = await _booksRepository.GetBooksAsync();
+            return Ok(bookEntities);
+        }
+    }
+}
