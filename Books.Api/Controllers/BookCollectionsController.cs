df98ec45dd484fea400536c2f50b787cbedb0dcb
Matt Wagner <matt.wagner@tcgplayer.com>
2018-10-08 21:47:22 -0400

    Add support for bulk inserts

---
+++ Books.Api/Controllers/BookCollectionsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Books.Api.Models;
+using Books.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Books.Api.Controllers
+{
+    [Route("api/bookcollections")]
+    [ApiController]
+    public class BookCollectionsController : ControllerBase
+    {
+        private readonly IBooksRepository _booksRepository;
+        private readonly IMapper _mapper;
+
+        public BookCollectionsController(IBooksRepository booksRepository, IMapper mapper)
+        {
+            _booksRepository = booksRepository ?? throw new ArgumentNullException(nameof(booksRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBookCollection([FromBody] IEnumerable<BookForCreation> bookCollection)
+        {
+            var bookEntities = _mapper.Map<IEnumerable<Entities.Book>>(bookCollection);
+
+            foreach (var bookEntity in bookEntities)
+            {
+                _booksRepository.AddBook(bookEntity);
+            }
+
+            await _booksRepository.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
