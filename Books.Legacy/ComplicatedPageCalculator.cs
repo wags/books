5d7084e287cdc64a05c1db7294367337092af395
Matt Wagner <matt.wagner@tcgplayer.com>
2018-10-14 22:02:19 -0400

    Add Books.Legacy project
    This contains code to mimic non-async, legacy code.

---
+++ Books.Legacy/ComplicatedPageCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Books.Legacy
+{
+    /// <summary>
+    /// Simulate long-running legacy, non-async code
+    /// </summary>
+    public class ComplicatedPageCalculator
+    {
+        /// <summary>
+        /// Full CPU load for 5 seconds
+        /// </summary>
+        public int CalculateBookPages()
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+            while (true)
+            {
+                if (watch.ElapsedMilliseconds > 5000)
+                {
+                    break;
+                }
+            }
+
+            return 42;
+        }
+    }
+}
